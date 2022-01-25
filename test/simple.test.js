941dd3707289476d35d9b83e05127f05fbfcf91e
TaseenAnsari <63318687+TaseenAnsari@users.noreply.github.com>
2022-01-25 11:30:52 +0530

    Update simple.test.js

--- test/simple.test.js
+++ test/simple.test.js
@@ -1,7 +1,7 @@
 describe("server test", () => {
   describe("starting", () => {
     it("should return object", async () => {
-      expect("helloworld").toBe("helloworld");
+      expect("helloworld").toBe("helloworldss");
     });
   });
 });
